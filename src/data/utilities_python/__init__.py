f6516d30fe942c9934c19ff19ba73cc79f048549
Victor Calderon <victor.calderon90@gmail.com>
2018-03-22 07:19:38 -0500

    Added 'pydotplus' and 'graphviz'

--- src/data/utilities_python/__init__.py
+++ src/data/utilities_python/__init__.py
@@ -2,14 +2,15 @@
 import warnings
 warnings.filterwarnings("ignore",category=FutureWarning)
 
-from .abundance_matching_vc import *
-from .file_dir_check        import *
-from .file_readers          import *
-from .geometry              import *
-from .get_path              import *
-from .magnitude_calc        import *
-from .pandas_hdf5           import *
-from .sdss_catls_obs        import *
-from .spherematch           import *
-from .statistics_vc         import *
+from .abundance_matching_vc  import *
+from .file_dir_check         import *
+from .file_readers           import *
+from .geometry               import *
+from .get_path               import *
+from .magnitude_calc         import *
+from .pandas_hdf5            import *
+from .sdss_catls_obs         import *
+from .spherematch            import *
+from .statistics_vc          import *
+from .adspy_shared_utilities import *
 
