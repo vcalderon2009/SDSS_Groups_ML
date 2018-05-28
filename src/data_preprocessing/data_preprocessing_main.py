13ec95bacc6b8311affc4c51c4d1719be9d9918e
Victor Calderon <victor.calderon90@gmail.com>
2018-05-28 14:16:36 -0500

    Not executing the main file

--- src/data_preprocessing/data_preprocessing_main.py
+++ src/data_preprocessing/data_preprocessing_main.py
@@ -561,7 +561,7 @@ def file_construction_and_execution(df_arr, param_dict):
     ##
     ## Running script
     print(".>>> Running Script....")
-    os.system("{0}".format(outfile_path))
+    # os.system("{0}".format(outfile_path))
 
 def main(args):
     """
