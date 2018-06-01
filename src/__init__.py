b4118d39659e7d9eee7d3cb918827dba81e0d1e0
Victor Calderon <victor.calderon90@gmail.com>
2018-06-01 11:38:33 -0500

    Added modules to main `src` to act as a package

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Victor Calderon
+# Created      : DATE
+# Last Modified: DATE
+# Vanderbilt University
+from __future__ import print_function, division, absolute_import
+__author__     = ['Victor Calderon']
+__copyright__  = ["Copyright 2018 Victor Calderon, "]
+__email__      = ['victor.calderon@vanderbilt.edu']
+__maintainer__ = ['Victor Calderon']
+"""
+Init file for main Source file
+"""
+
+# Importing modules
+
+from . import data
+from . import data_analysis
+from . import data_preprocessing
+from . import data_testing
