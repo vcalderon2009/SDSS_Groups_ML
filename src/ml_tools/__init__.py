2bcd4058253f16e230f6149af352d2e4526ff86f
Victor Calderon <victor.calderon90@gmail.com>
2018-06-02 17:40:18 -0500

    Modified the __init__.py for `ml_tools`

---
+++ src/ml_tools/__init__.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Victor Calderon
+# Created      : DATE
+# Last Modified: DATE
+# Vanderbilt University
+__author__     = ['Victor Calderon']
+__copyright__  = ["Copyright 2018 Victor Calderon, "]
+__email__      = ['victor.calderon@vanderbilt.edu']
+__maintainer__ = ['Victor Calderon']
+"""
+Init file for main Source file
+"""
+
+# Importing modules
+from .read_ml_catalogues import ReadML
