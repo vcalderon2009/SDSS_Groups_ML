3c0d02bcc02f6e1663b2b706723cdbb4e5820dba
Victor Calderon <victor.calderon90@gmail.com>
2018-06-13 12:36:20 +0800

    Plotting everything

--- src/data_analysis/catl_algorithm_comparison_plots.py
+++ src/data_analysis/catl_algorithm_comparison_plots.py
@@ -814,9 +814,9 @@ def frac_diff_model(models_dict, param_dict, proj_dict, plot_opt='mhalo',
                                                 mass_opt='dyn',
                                                 return_frac_diff=True,
                                                 nlim_threshold=True,
-                                                nlim_min=10)
+                                                nlim_min=4)
     # Only choosing non-zero values
-    dyn_pred_mask = dyn_pred >= 12.0
+    dyn_pred_mask = dyn_pred >= 11.0
     dyn_pred      = dyn_pred[dyn_pred_mask]
     dyn_true      = dyn_true[dyn_pred_mask]
     dyn_frac_diff = dyn_frac_diff[dyn_pred_mask]
@@ -1365,10 +1365,10 @@ def pred_masses_halo_mass(models_dict, param_dict, proj_dict,
         dyn_true,
         dyn_frac_diff) = param_dict['ml_args'].extract_trad_masses_alt(
                             mass_opt='dyn', return_frac_diff=True,
-                            nlim_threshold=True, nlim_min=10)
+                            nlim_threshold=True, nlim_min=4)
     #
     # Only choosing non-zero values and those larger than `10`
-    dyn_pred_mask = dyn_pred >= 12.0
+    dyn_pred_mask = dyn_pred >= 11.0
     dyn_pred      = dyn_pred[dyn_pred_mask]
     dyn_true      = dyn_true[dyn_pred_mask]
     dyn_frac_diff = dyn_frac_diff[dyn_pred_mask]
