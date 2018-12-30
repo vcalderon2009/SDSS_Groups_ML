35e0d4a7c75c4530d9f54472aabd99142830e85b
Victor Calderon <victor.calderon90@gmail.com>
2018-12-30 17:00:12 +0100

    Removed 'g_brightest' from Figure 1

--- src/data_preprocessing/catl_feature_general_plots.py
+++ src/data_preprocessing/catl_feature_general_plots.py
@@ -1086,7 +1086,7 @@ def covariance_plot(catl_pd, param_dict, proj_dict, plot_only_feat=False,
     ## Renaming properties
     catl_pd_copy = catl_pd.copy()
     ## Dropping columns
-    cols_drop = ['GG_pointing', 'GG_haloid_point', 'GG_mhalo_point']
+    cols_drop = ['GG_pointing', 'GG_haloid_point', 'GG_mhalo_point', 'g_brightest']
     catl_pd_copy.drop(cols_drop, axis=1, inplace=True)
     ## Reordering columns
     mhalo_key = 'M_h'
